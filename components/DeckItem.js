acaa6f4ea06dc5bd269f2a9ff4402d1de43215b5
mark.ang <mark.ang@incognito.com>
2018-11-01 19:07:21 -0700

    Removed commented out code

--- components/DeckItem.js
+++ components/DeckItem.js
@@ -6,7 +6,7 @@ export default class DeckItem extends Component {
     render() {
         const { title, cards } = this.props 
         return(
-            <View style={styles.container}>
+            <View>
                 <Text style={styles.title}>{title}</Text>
                 <Text style={styles.cards}>{cards} {cards > 1 ? "cards" : "card"}</Text>
             </View>
@@ -15,22 +15,6 @@ export default class DeckItem extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        // backgroundColor: white,
-        // borderRadius: 2,
-        // padding: 20,
-        // marginLeft: 10,
-        // marginRight: 10,
-        // marginTop: 17,
-        // justifyContent: 'center',
-        // shadowRadius: 3,
-        // shadowOpacity: 0.8,
-        // shadowColor: 'rgba(0, 0, 0, 0.24)',
-        // shadowOffset: {
-        //     width: 0,
-        //     height: 3
-        // }
-    },
     title: {
         fontSize: 30,
         textAlign: 'center'
